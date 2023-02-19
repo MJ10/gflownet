187bd0421132fbc130c3395ce9f5885e74106444
Moksh Jain <mokshjn00@gmail.com>
2023-02-18 22:46:40 -0500

    add tdc task

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ pip install --no-index --find-links ~/wheels/ ~/wheels/torch-1.13.0+cu117-cp39-c
 pip install --no-index --find-links ~/wheels/ ~/wheels/torch_scatter-2.0.9-cp39-cp39-linux_x86_64.whl
 pip install --no-index --find-links ~/wheels/ ~/wheels/torch_sparse-0.6.15-cp39-cp39-linux_x86_64.whl
 pip install --no-index --find-links ~/wheels/ ~/wheels/torch_cluster-1.6.0-cp39-cp39-linux_x86_64.whl
-pip install --no-index --find-links ~/wheels/ torch-geometric botorch cvxopt tensorboard
+pip install --no-index --find-links ~/wheels/ torch-geometric botorch cvxopt tensorboard pytdc
 cd ~/recursion/gflownet/
 pip install --no-index --find-links ~/wheels/ -e .
 export GFN_MODEL_PATH=/scratch/mjain/mol_gen/best_params.pkl.gz
---
+++ src/gflownet/tasks/tdc_opt.py
@@ -0,0 +1,179 @@
+import ast
+import copy
+from typing import Any, Callable, Dict, List, Tuple, Union
+
+import numpy as np
+from rdkit import RDLogger
+from rdkit.Chem.rdchem import Mol as RDMol
+from rdkit.Chem.rdmolfiles import MolToSmiles
+import scipy.stats as stats
+import torch
+from torch import Tensor
+import torch.nn as nn
+from torch.utils.data import Dataset
+import torch_geometric.data as gd
+
+from gflownet.algo.trajectory_balance import TrajectoryBalance
+from gflownet.envs.frag_mol_env import FragMolBuildingEnvContext
+from gflownet.envs.graph_building_env import GraphBuildingEnv
+from gflownet.models import bengio2021flow
+from gflownet.models.graph_transformer import GraphTransformerGFN
+from gflownet.train import FlatRewards
+from gflownet.train import GFNTask
+from gflownet.train import GFNTrainer
+from gflownet.train import RewardScalar
+from gflownet.utils.transforms import thermometer
+from tdc import Oracle
+
+class TDCTask(GFNTask):
+    """
+    Task where the reward is computed using oracles from TDC
+    """
+    def __init__(self, name: str, temperature_distribution: str, temperature_parameters: Tuple[float]):
+        self.oracle = Oracle(name=name)
+        self.temperature_sample_dist = temperature_distribution
+        self.temperature_dist_params = temperature_parameters
+    
+    def sample_conditional_information(self, n):
+        beta = None
+        if self.temperature_sample_dist == 'gamma':
+            loc, scale = self.temperature_dist_params
+            beta = self.rng.gamma(loc, scale, n).astype(np.float32)
+            upper_bound = stats.gamma.ppf(0.95, loc, scale=scale)
+        elif self.temperature_sample_dist == 'uniform':
+            beta = self.rng.uniform(*self.temperature_dist_params, n).astype(np.float32)
+            upper_bound = self.temperature_dist_params[1]
+        elif self.temperature_sample_dist == 'beta':
+            beta = self.rng.beta(*self.temperature_dist_params, n).astype(np.float32)
+            upper_bound = 1
+        beta_enc = thermometer(torch.tensor(beta), 32, 0, upper_bound)  # TODO: hyperparameters
+        return {'beta': torch.tensor(beta), 'encoding': beta_enc}
+
+    def cond_info_to_reward(self, cond_info: Dict[str, Tensor], flat_reward: FlatRewards) -> RewardScalar:
+        if isinstance(flat_reward, list):
+            flat_reward = torch.tensor(flat_reward)
+        return flat_reward.flatten()**cond_info['beta']
+
+    def compute_flat_rewards(self, mols: List[RDMol]) -> Tuple[FlatRewards, Tensor]:
+        smiles = [MolToSmiles(i) for i in mols]
+        is_valid = torch.tensor([i is not None or i != '' for i in smiles]).bool()
+        if not is_valid.any():
+            return FlatRewards(torch.zeros((0, 1))), is_valid
+        #batch = gd.Batch.from_data_list([i for i in graphs if i is not None])
+        #batch.to(self.device)
+        #preds = self.models['seh'](batch).reshape((-1,)).data.cpu()
+        #preds[preds.isnan()] = 0
+        #preds = self.flat_reward_transform(preds).clip(1e-4, 100).reshape((-1, 1))
+        scores = torch.tensor(self.oracle(smiles)).reshape((-1, 1))    
+        print(scores)
+        return FlatRewards(scores), is_valid
+
+
+class TDCFragTrainer(GFNTrainer):
+    def default_hps(self) -> Dict[str, Any]:
+        return {
+            'bootstrap_own_reward': False,
+            'learning_rate': 1e-4,
+            'global_batch_size': 64,
+            'num_emb': 128,
+            'num_layers': 4,
+            'tb_epsilon': None,
+            'illegal_action_logreward': -75,
+            'reward_loss_multiplier': 1,
+            'temperature_sample_dist': 'uniform',
+            'temperature_dist_params': '(.5, 32)',
+            'weight_decay': 1e-8,
+            'num_data_loader_workers': 4,
+            'momentum': 0.9,
+            'adam_eps': 1e-8,
+            'lr_decay': 20000,
+            'Z_lr_decay': 20000,
+            'clip_grad_type': 'norm',
+            'clip_grad_param': 10,
+            'random_action_prob': 0.,
+            'sampling_tau': 0.,
+            'num_cond_dim': 32,
+            'oracle_name': 'celecoxib_rediscovery'
+        }
+
+    def setup_algo(self):
+        self.algo = TrajectoryBalance(self.env, self.ctx, self.rng, self.hps, max_nodes=9)
+
+    def setup_task(self):
+        self.task = TDCTask(self.hps['oracle_name'], self.hps['temperature_sample_dist'],
+                            ast.literal_eval(self.hps['temperature_dist_params']))
+
+    def setup_model(self):
+        self.model = GraphTransformerGFN(self.ctx, num_emb=self.hps['num_emb'], num_layers=self.hps['num_layers'])
+
+    def setup(self):
+        hps = self.hps
+        RDLogger.DisableLog('rdApp.*')
+        self.rng = np.random.default_rng(142857)
+        self.env = GraphBuildingEnv()
+        self.ctx = FragMolBuildingEnvContext(max_frags=9, num_cond_dim=hps['num_cond_dim'])
+        self.training_data = []
+        self.test_data = []
+        self.offline_ratio = 0
+        self.valid_offline_ratio = 0
+        self.setup_algo()
+        self.setup_task()
+        self.setup_model()
+
+        # Separate Z parameters from non-Z to allow for LR decay on the former
+        Z_params = list(self.model.logZ.parameters())
+        non_Z_params = [i for i in self.model.parameters() if all(id(i) != id(j) for j in Z_params)]
+        self.opt = torch.optim.Adam(non_Z_params, hps['learning_rate'], (hps['momentum'], 0.999),
+                                    weight_decay=hps['weight_decay'], eps=hps['adam_eps'])
+        self.opt_Z = torch.optim.Adam(Z_params, hps['learning_rate'], (0.9, 0.999))
+        self.lr_sched = torch.optim.lr_scheduler.LambdaLR(self.opt, lambda steps: 2**(-steps / hps['lr_decay']))
+        self.lr_sched_Z = torch.optim.lr_scheduler.LambdaLR(self.opt_Z, lambda steps: 2**(-steps / hps['Z_lr_decay']))
+
+        self.sampling_tau = hps['sampling_tau']
+        if self.sampling_tau > 0:
+            self.sampling_model = copy.deepcopy(self.model)
+        else:
+            self.sampling_model = self.model
+        eps = hps['tb_epsilon']
+        hps['tb_epsilon'] = ast.literal_eval(eps) if isinstance(eps, str) else eps
+
+        self.mb_size = hps['global_batch_size']
+        self.clip_grad_param = hps['clip_grad_param']
+        self.clip_grad_callback = {
+            'value': (lambda params: torch.nn.utils.clip_grad_value_(params, self.clip_grad_param)),
+            'norm': (lambda params: torch.nn.utils.clip_grad_norm_(params, self.clip_grad_param)),
+            'none': (lambda x: None)
+        }[hps['clip_grad_type']]
+
+    def step(self, loss: Tensor):
+        loss.backward()
+        for i in self.model.parameters():
+            self.clip_grad_callback(i)
+        self.opt.step()
+        self.opt.zero_grad()
+        self.opt_Z.step()
+        self.opt_Z.zero_grad()
+        self.lr_sched.step()
+        self.lr_sched_Z.step()
+        if self.sampling_tau > 0:
+            for a, b in zip(self.model.parameters(), self.sampling_model.parameters()):
+                b.data.mul_(self.sampling_tau).add_(a.data * (1 - self.sampling_tau))
+
+
+def main():
+    """Example of how this model can be run outside of Determined"""
+    hps = {
+        'lr_decay': 10000,
+        'log_dir': '/scratch/mjain/logs/seh_frag/',
+        'num_training_steps': 10_000,
+        'validate_every': 500,
+        'sampling_tau': 0.99,
+        'temperature_dist_params': '(0, 64)',
+    }
+    trial = TDCFragTrainer(hps, torch.device('cuda'))
+    trial.verbose = True
+    trial.run()
+
+
+if __name__ == '__main__':
+    main()
